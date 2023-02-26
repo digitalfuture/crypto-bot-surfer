39f748c412fff82ace90df95925201fc0ebbe743
avorio <avorio@mail.ru>
2023-02-25 22:58:28 -0500

    Queue existense check

--- server/api/binance/queue.js
+++ server/api/binance/queue.js
@@ -2,7 +2,7 @@ import amqp from "amqplib";
 
 const RABBITMQ_HOST = process.env.RABBITMQ_HOST || "localhost";
 const RABBITMQ_QUEUE_NAME = process.env.RABBITMQ_QUEUE_NAME || "requests";
-const DELAY_MS = process.env.DELAY_MS || 200;
+const PROCESS_DELAY = process.env.PROCESS_DELAY || 100;
 
 let channel;
 
@@ -21,18 +21,30 @@ async function setupQueue() {
 
 async function queue(request) {
   try {
+    if (!channel) {
+      throw new Error("Queue is not available at the moment");
+    }
+
     const message = JSON.stringify(request());
     channel.sendToQueue(RABBITMQ_QUEUE_NAME, Buffer.from(message), {
       persistent: true,
     });
 
+    // console.log("Message sent to queue:", message);
+
     return new Promise(function (resolve) {
-      setTimeout(() => {
-        channel.consume(RABBITMQ_QUEUE_NAME, function (message) {
+      setTimeout(async function () {
+        const message = await channel.get(RABBITMQ_QUEUE_NAME, {
+          noAck: false,
+        });
+        if (message) {
+          // console.log("Message received from queue:", message.content.toString());
           resolve(JSON.parse(message.content.toString()));
           channel.ack(message);
-        });
-      }, DELAY_MS);
+        } else {
+          resolve();
+        }
+      }, PROCESS_DELAY);
     });
   } catch (err) {
     console.error("Queue is not available at the moment");
