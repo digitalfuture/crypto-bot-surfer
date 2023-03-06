4e3bf508b78c72aca970df0db6e7248480ea6631
avorio <avorio@mail.ru>
2023-03-05 22:17:48 -0500

    queue env log

--- server/api/binance/connection.js
+++ server/api/binance/connection.js
@@ -5,6 +5,8 @@ const useQueue = JSON.parse(process.env.USE_QUEUE);
 const isTestMode = JSON.parse(process.env.TEST_MODE);
 const queueUrl = process.env.QUEUE_URL;
 
+console.log("Queue URL:", queueUrl);
+
 const options = {
   APIKEY: process.env.BINANCE_APIKEY,
   APISECRET: process.env.BINANCE_APISECRET,
