5670b10fc8f8bb0acf6af2188fa2c8efbfb23950
avorio <avorio@mail.ru>
2023-03-05 21:26:53 -0500

    env parse fix

--- server/api/binance/connection.js
+++ server/api/binance/connection.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const useQueue = JSON.parse(process.env.USE_QUEUE);
 const isTestMode = JSON.parse(process.env.TEST_MODE);
-const queueUrl = JSON.parse(process.env.QUEUE_URL);
+const queueUrl = process.env.QUEUE_URL;
 
 const options = {
   APIKEY: process.env.BINANCE_APIKEY,
