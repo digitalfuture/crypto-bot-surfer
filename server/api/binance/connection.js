cec0d19c00aa53b495000ca95505040d36b614cc
avorio <avorio@mail.ru>
2023-03-26 16:01:17 -0500

    Logs - update

--- server/api/binance/connection.js
+++ server/api/binance/connection.js
@@ -5,8 +5,6 @@ const useQueue = JSON.parse(process.env.USE_QUEUE);
 const isTestMode = JSON.parse(process.env.TEST_MODE);
 const queueUrl = process.env.QUEUE_URL;
 
-console.log("Queue URL:", queueUrl);
-
 const options = {
   APIKEY: process.env.BINANCE_APIKEY,
   APISECRET: process.env.BINANCE_APISECRET,
@@ -19,6 +17,8 @@ const options = {
 let binance = new Binance().options(options);
 
 if (useQueue) {
+  console.log("Queue URL:", queueUrl);
+
   axios.interceptors.request.use(async (config) => {
     try {
       const response = await axios.post(queueUrl, {
