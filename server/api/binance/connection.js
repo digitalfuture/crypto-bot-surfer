7089bfc4fb17fe22dbb7afcf9db4ae70681d5e89
avorio <avorio@mail.ru>
2023-03-26 20:41:51 -0500

    Connection -update

--- server/api/binance/connection.js
+++ server/api/binance/connection.js
@@ -4,8 +4,6 @@ const useQueue = JSON.parse(process.env.USE_QUEUE);
 const isTestMode = JSON.parse(process.env.TEST_MODE);
 const queueUrl = process.env.QUEUE_URL;
 
-console.log("Queue URL:", queueUrl);
-
 const options = {
   APIKEY: process.env.BINANCE_APIKEY,
   APISECRET: process.env.BINANCE_APISECRET,
@@ -18,13 +16,15 @@ const options = {
 let binance = new Binance().options(options);
 
 if (useQueue) {
+  console.log("Queue URL:", queueUrl);
+
   binance._makeRequest = async function (
     path,
     data = {},
     method = "GET",
     headers = {}
   ) {
-    const response = await fetch(`${queueUrl}/queue/binance`, {
+    const response = await fetch(`${queueUrl}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ path, data, method, headers }),
