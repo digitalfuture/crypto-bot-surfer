8c27a9991a677734d351ee709ebacd7e4255d518
avorio <avorio@mail.ru>
2023-03-30 15:32:31 -0500

    Refactoring

--- server/analytics/indicators/index.js
+++ server/analytics/indicators/index.js
@@ -1,9 +1,6 @@
 import * as dumpBasic from "./dump-basic.js";
-import * as pumpBasic from "./pump-basic.js";
 import * as dumpTrailing from "./dump-trailing.js";
-import * as pumpTrailing from "./pump-trailing.js";
 import * as dumpTrailingBtcFilter from "./dump-trailing--btc-filter.js";
-import * as pumpTrailingBtcFilter from "./pump-trailing--btc-filter.js";
 
 const indicatorName = process.env.INDICATOR;
 
@@ -13,20 +10,11 @@ switch (indicatorName) {
   case "dump-basic":
     getTradeSignals = dumpBasic.getTradeSignals;
     break;
-  case "pump-basic":
-    getTradeSignals = pumpBasic.getTradeSignals;
-    break;
   case "dump-trailing":
     getTradeSignals = dumpTrailing.getTradeSignals;
     break;
-  case "pump-trailing":
-    getTradeSignals = pumpTrailing.getTradeSignals;
-    break;
   case "dump-trailing--btc-filter":
     getTradeSignals = dumpTrailingBtcFilter.getTradeSignals;
-    break;
-  case "pump-trailing--btc-filter":
-    getTradeSignals = pumpTrailingBtcFilter.getTradeSignals;
     break;
 }
 
