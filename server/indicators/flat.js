c66b2bc0fe6e5c7dd55e076c997ceadbe89b1fcf
avorio <avorio@mail.ru>
2023-04-14 19:19:33 -0500

    Refactor

---
+++ server/indicators/flat.js
@@ -0,0 +1,142 @@
+import { getPrevDayData, getTradingTickers } from "../api/binance/info.js";
+import { getLastPrice } from "../api/binance/info.js";
+
+let lastTickerListToBuy = null;
+
+export async function getTradeSignals({
+  secondarySymbol,
+  currentSymbol,
+  lastTrade,
+  lastCheck,
+}) {
+  try {
+    // console.info("\nlastCheck:", lastCheck);
+    // console.info("lastTrade:", lastTrade);
+
+    const tradingTickers = await getTradingTickers();
+    // console.info("tradingTickers:", tradingTickers);
+
+    const priceListData = await getPrevDayData();
+    // console.info("priceListData:", priceListData);
+
+    const mappedList = priceListData.map(
+      ({
+        symbol,
+        priceChangePercent,
+        lastPrice,
+        openTime,
+        closeTime,
+        ...others
+      }) => ({
+        primarySymbol: symbol.split(secondarySymbol)[0],
+        secondarySymbol,
+        tickerName: symbol,
+        priceChangePercent: parseFloat(priceChangePercent),
+        lastPrice: parseFloat(lastPrice),
+        openTime,
+        closeTime,
+        ...others,
+      })
+    );
+
+    const tickerListToBuy = mappedList
+      .filter(({ tickerName }) => tickerName.endsWith(secondarySymbol))
+      .filter(({ primarySymbol }) => !primarySymbol.endsWith("DOWN"))
+      .filter(({ primarySymbol }) => !primarySymbol.endsWith("UP"))
+      .filter(({ primarySymbol }) =>
+        tradingTickers.includes(primarySymbol + secondarySymbol)
+      )
+      .filter(({ primarySymbol }) => primarySymbol !== lastTrade.symbol)
+      .filter(({ primarySymbol }) => primarySymbol !== currentSymbol)
+      .sort((a, b) => b.priceChangePercent - a.priceChangePercent);
+
+    lastTickerListToBuy = tickerListToBuy;
+
+    //
+    // Buy signal
+    const tickerToBuy = tickerListToBuy[tickerListToBuy.length - 1];
+    const buyPrimarySymbol = tickerToBuy.primarySymbol;
+    const buyTickerName = tickerToBuy.tickerName;
+    const buyPrice = tickerToBuy && parseFloat(tickerToBuy.lastPrice);
+    const buyTickerPriceChangePercent = tickerToBuy.priceChangePercent;
+    const buyCondition = !currentSymbol;
+    const isBuySignal = buyCondition;
+
+    //
+    // Sell signal
+    const tickerToSell = mappedList.find(
+      ({ primarySymbol }) => primarySymbol === currentSymbol
+    );
+
+    const sellPrimarySymbol = tickerToSell?.primarySymbol;
+    const sellTickerName = tickerToSell?.tickerName;
+    const sellPrice = tickerToSell && parseFloat(tickerToSell.lastPrice);
+    const sellTickerPriceChangePercent = tickerToSell?.priceChangePercent;
+    const sellCondition1 = lastCheck.symbol === currentSymbol;
+    const sellCondition2 = sellPrice < lastCheck.price;
+    const isSellSignal = sellCondition1 && sellCondition2;
+
+    // BTC / USDT
+    const btcUsdtPrice = await getLastPrice("BTCUSDT");
+
+    // Market average
+    const filteredListForMarketChange = mappedList
+      .filter(({ tickerName }) => tickerName.endsWith(secondarySymbol))
+      .filter(({ primarySymbol }) => !primarySymbol.endsWith("DOWN"))
+      .filter(({ primarySymbol }) => !primarySymbol.endsWith("UP"))
+      .filter(({ primarySymbol }) =>
+        tradingTickers.includes(primarySymbol + secondarySymbol)
+      );
+
+    const marketAveragePrice = filteredListForMarketChange.reduce(
+      (sum, { lastPrice }, index, array) => {
+        sum = sum + parseFloat(lastPrice);
+
+        if (index === array.length - 1) {
+          return sum / array.length;
+        } else {
+          return sum;
+        }
+      },
+      0
+    );
+
+    //
+    // Result
+    const result = {
+      sellPrimarySymbol,
+      buyPrimarySymbol,
+      sellTickerName,
+      buyTickerName,
+      buyPrice,
+      sellPrice,
+      buyTickerPriceChangePercent,
+      sellTickerPriceChangePercent,
+      isBuySignal,
+      isSellSignal,
+      btcUsdtPrice,
+      marketAveragePrice,
+    };
+
+    // console.info("\nCheck signals result:", {
+    //   buySignal: {
+    //     buyPrimarySymbol,
+    //     buyTickerName,
+    //     buyPrice,
+    //     buyTickerPriceChangePercent,
+    //     isBuySignal,
+    //   },
+    //   sellSignal: {
+    //     sellPrimarySymbol,
+    //     sellTickerName,
+    //     sellPrice,
+    //     sellTickerPriceChangePercent,
+    //     isSellSignal,
+    //   },
+    // });
+
+    return result;
+  } catch (error) {
+    throw { type: "Get Trade Signals Error", ...error, errorSrcData: error };
+  }
+}
--- server/indicators/index.js
+++ server/indicators/index.js
@@ -1,4 +1,5 @@
 import * as indicatorDump from "./dump.js";
+import * as indicatorFlat from "./flat.js";
 import * as indicatorPump from "./pump.js";
 
 const indicatorName = process.env.INDICATOR;
@@ -9,7 +10,10 @@ switch (indicatorName) {
   case "dump":
     getTradeSignals = indicatorDump.getTradeSignals;
     break;
-  case "pump":
+  case "flat":
+    getTradeSignals = indicatorPump.getTradeSignals;
+    break;
+  case "flat":
     getTradeSignals = indicatorPump.getTradeSignals;
     break;
 }
