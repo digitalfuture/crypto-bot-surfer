4723d3372931ec4bb3585263d6a0a39f49abf9fa
avorio <avorio@mail.ru>
2024-08-29 11:58:02 -0500

    Indicators - List - update

--- server/indicators/index.js
+++ server/indicators/index.js
@@ -3,7 +3,7 @@ import * as indicatorFlat from "./flat.js";
 import * as indicatorPump from "./pump.js";
 import * as indicatorSimple from "./simple.js";
 import * as indicatorExternal from "./external.js";
-import * as indicatorAi from "./ai.js";
+// import * as indicatorAi from "./ai.js";
 
 const indicatorName = process.env.INDICATOR;
 
@@ -25,9 +25,9 @@ switch (indicatorName) {
   case "external":
     getTradeSignals = indicatorExternal.getTradeSignals;
     break;
-  case "ai":
-    getTradeSignals = indicatorAi.getTradeSignals;
-    break;
+  // case "ai":
+  //   getTradeSignals = indicatorAi.getTradeSignals;
+  //   break;
 }
 
 export const getSignals = getTradeSignals;
