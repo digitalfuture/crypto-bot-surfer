8456b48e99050f4efebf7122617eb17e9034c965
avorio <avorio@mail.ru>
2024-12-05 13:02:17 -0500

    Docker - update

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-docker rm $(docker ps -aqf "name=crypto-bot-surfer*") --force
+containers=$(docker ps -aqf "name=crypto-bot-surfer*")
+
+if [ -n "$containers" ]; then
+  docker rm $containers --force
+else
+  echo "No containers to remove."
+fi
 
 docker compose up
