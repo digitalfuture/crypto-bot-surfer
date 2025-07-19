ac4984f71579397f4bde5647dde1e818793511e3
avorio <avorio@mail.ru>
2025-07-19 18:27:08 -0500

    Filter - update

--- server/analytics/indicators/algorithms/volatility.js
+++ server/analytics/indicators/algorithms/volatility.js
@@ -51,6 +51,9 @@ export async function getTradeSignals(state = {}) {
         };
       })
       .filter(({ symbol }) => symbol.endsWith(secondarySymbol))
+      .filter(({ primarySymbol }) => !primarySymbol.endsWith("DOWN"))
+      .filter(({ primarySymbol }) => !primarySymbol.endsWith("UP"))
+      .filter(({ primarySymbol }) => primarySymbol.startsWith("USD"))
       .filter(({ primarySymbol, secondarySymbol }) =>
         tradingTickersFutures.includes(primarySymbol + secondarySymbol)
       )
